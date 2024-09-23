02b5b1002f5dbddae9504bcb2e001e931fe2d447
Vitor Daniel <vitor036daniel@gmail.com>
2024-09-23 14:18:06 -0300

    prisma

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
+  console.log("Hello World")
   const allUsers = await prisma.user.findMany()
   console.log(allUsers)
 }
